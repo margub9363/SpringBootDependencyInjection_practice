f332e1cb95d849367d54b2811043e55d637d0e21
margub9363 <69838623+margub9363@users.noreply.github.com>
2023-07-29 16:12:35 +0530

    Resolving the issue by making use of Qualifier

--- DependencyInjectionPractice/src/main/java/com/example/DependencyInjectionPractice/binarySerach/BinarySerachimpl.java
+++ DependencyInjectionPractice/src/main/java/com/example/DependencyInjectionPractice/binarySerach/BinarySerachimpl.java
@@ -1,12 +1,14 @@
 package com.example.DependencyInjectionPractice.binarySerach;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BinarySerachimpl {
 
     @Autowired
+    @Qualifier("quickSortAlgorithm")
     private SortAlgorithm sortAlgorithm;
 
     public int binarySearch(int[] numbers, int numberToSearch) {
