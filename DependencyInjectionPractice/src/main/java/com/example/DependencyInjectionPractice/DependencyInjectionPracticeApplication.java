bc62340c07e150053a36fd995d1e5b581b143c11
margub9363 <69838623+margub9363@users.noreply.github.com>
2023-07-29 12:41:08 +0530

    Basic Project Setup

---
+++ DependencyInjectionPractice/src/main/java/com/example/DependencyInjectionPractice/DependencyInjectionPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.DependencyInjectionPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependencyInjectionPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DependencyInjectionPracticeApplication.class, args);
+	}
+
+}
---
+++ DependencyInjectionPractice/src/test/java/com/example/DependencyInjectionPractice/DependencyInjectionPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DependencyInjectionPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependencyInjectionPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
